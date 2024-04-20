03cc59a561daf3011b8b95a81bce005d75af68c9
Kartik Donwade <kartikdonwade45@gmail.com>
2024-04-20 23:47:11 +0530

    app changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
 
 import './App.css';
+import {createBrowserRouter} from './BrowserRouter.js';
+
 
 function App() {
+  const route = createBrowserRouter(
+[{
+  path:"/",
+  element:"Home Page",
+},
+{
+  path:"/add",
+  element:"User add ",
+},
+{
+  path:"/edit",
+  element:"Edit page",
+},
+]
+  );
   return (
     <div className="App">
 Hi
