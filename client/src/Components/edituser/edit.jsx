4c6a48e18d3a3f4938df1716683ac2ee417850f9
Kartik Donwade <kartikdonwade45@gmail.com>
2024-04-26 13:53:40 +0530

    project completed

--- client/src/Components/edituser/edit.jsx
+++ client/src/Components/edituser/edit.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import {Link,  useParams} from 'react-router-dom'
+import {Link,  useParams, useNavigate} from 'react-router-dom'
 import "./edit.css"
 import axios from 'axios'
+import toast from 'react-hot-toast';
 const Edit = () => {
 const users ={
   fname:"",
@@ -9,7 +10,7 @@ const users ={
   email:""
 }
  const {id} = useParams();
- 
+ const navigate =useNavigate();
  const [user, setUser] = useState(users);
  const inputchanged = (e) => {
   const {name, value} = e.target;
@@ -25,12 +26,26 @@ setUser(res.data);
 });
 
   },[id])
+
+
+  const submitform= async (e) => {
+      e.preventDefault();
+    await axios.put(`http://localhost:8000/api/update/${id}`,user)
+    .then((res) => {
+    
+toast.success(res.data.message, {position:"top-right"})
+  
+ navigate('/')
+    }).catch(err => console.log(err));
+  }
+
+
   return (
     <div className='adduser'>
       <Link className='backbu' to="/">Back
     </Link>
     <h3>Update User</h3>
-    <form className='formadd'>
+    <form className='formadd' onSubmit={submitform}>
       <div className='inputf'><label>First Name</label>
       <input type="text" name="fname" value={user.fname} onChange={inputchanged} placeholder='First Name'/></div>
       <div className='inputf' >  <label>Last Name</label>
